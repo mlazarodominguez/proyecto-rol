2969731914e1debbbba2213713a385321147250e
José Leal <lealriveropepe@outlook.es>
2019-01-11 10:13:45 +0100

    Mordekaiser y Historia Kled

---
+++ ROL/src/vista/MensajeHitoriaKled.java
@@ -0,0 +1,49 @@
+package vista;
+
+public class MensajeHitoriaKled {
+	
+	public static void DescripcionKled() {
+		System.out.println(
+				"\nKled es un héroe popular entre las gentes de Valoran. Muchos soldados afirman \nque ha luchado en todos las guerras y en todas ha salido como vencedor.\n"
+						+ "Kled tiene un rol de luchador y es el mejor en el ataque cuerpo a cuerpo\n");
+	}
+
+	public static void HistoriaKled() {
+		System.out.println(
+				"\nUn día, de la nada, apareció Kled en lo alto de la cima, contemplando el campo de batalla. Iba a lomo de Skaarl, un dragarto inmortal del desierto."
+						+ "\nEl jinete solitario se puso de pie sobre la silla de su montura. ");
+
+	}
+
+	// TODO: pasar el mapa de Valoran si fuese posible para dar una idea al jugador
+
+	public static void HistoriaKled2() {
+		System.out.println("\nCuando Kled puso la vista en el campo de batalla vio algo que no le gustaría ver a nadie."
+				+ "\nNo tenía muchas opciones puesto que apenas tenía recursos."
+				+ "\nSe encontró con con una feroz criatura. Cuando se posó ante él se convirtió en humana. ¡Era Shyvana!"
+				+ "¿Qué podría hacer Kled, si no tenía recursos?");
+	}
+	
+	public static void HistoriaKled3() {
+		System.out.println("\nDías después, de camino a Shuryma, minetras Kled y Skaarl descansaban, algo o alguien rondaba por allí."
+				+ "\nCuando Kled se despertó se dio cuenta de que algo le faltaba pero no sabía que era."
+				+ "\nCuando estaba llamando a Skaarl, al ver que éste no aparecía cayó que era él lo que le faltaba.");		
+	}
+	
+	//TODO: poner una imagen de skaarl
+	
+	public static void HistoriaKled4() {
+		System.out.println("\nEmpezó a buscarlo pero tenía que seguir cuanto antes su camino porque algo bueno le esperaba en Shuryma."
+				+ "\nContinuó hacia su destino y cuando llegó se encontro con Skaarl.");
+				
+	}
+	
+	//TODO: a ser posible si se puede meter una imagen de skaarl y kled mirandose
+	
+	public static void HistoriaKled5() {
+		System.out.println("n-¿Qué haces aquí?-pregunta sorprendido y decepcionado Kled-"
+				+ "\n-No tuve otra opción-contestó entre lágrimas-" 
+				+ "");
+	}
+
+}
